d84b45b32044075ae9e9e9155088461cbcbba38b
toniya-klaytn <toniya.sun@klaytn.foundation>
2022-10-05 15:53:21 +0800

    ui changes

--- public/index.js
+++ public/index.js
@@ -9,30 +9,12 @@ document.getElementById("connectButton").onclick = async () => {
         let address = accounts[0];
         let balance = await caver.klay.getBalance(accounts[0]);
         balance = caver.utils.convertFromPeb(balance,"KLAY");
-        // await ethereum.request({
-        //     method: "wallet_enable",
-        //     params: [{ wallet_snap: { [snapId]: {} } }],
-        // });
-
-        // const [address, balance] = await Promise.all([
-        //     ethereum.request({
-        //         method: "wallet_invokeSnap",
-        //         params: [snapId, { method: "klay_getAddress" }],
-        //     }),
-        //     ethereum.request({
-        //         method: "wallet_invokeSnap",
-        //         params: [
-        //             snapId,
-        //             { method: "klay_getBalance", params: { network } },
-        //         ],
-        //     }),
-        // ]);
 
         document.getElementById("addressSpan").innerText = address;
         document.getElementById("from").value = address;
         
         document.getElementById("balanceSpan").innerText = balance;
-        document.getElementById("connectButton").innerHTML = `<span>●</span> Baobab Network`;
+        document.getElementById("connectButton").innerHTML = `Baobab Network`;
         document.getElementById("gaslessFeature").style.display = 'block';
         document.getElementsByClassName('BytecodeExample__code')[0].innerHTML = byteCode;
         document.getElementsByClassName('AbiExample__code')[0].innerHTML = JSON.stringify(abi, null, 4);
@@ -49,16 +31,6 @@ const valueString = 'valueString';
 document.getElementById("signDeployerButton").onclick = async () => {
     // Deploying the smart contract with initial values
     try {
-        // const signedMessage = await ethereum.request({
-        //     method: "wallet_invokeSnap",
-        //     params: [
-        //         snapId,
-        //         {
-        //             method: "klay_signDeployerMessage",
-        //             params: { byteCode, abi, network, keyString, valueString },
-        //         },
-        //     ],
-        // });
 
         let data = byteCode + caver.abi.encodeParameters(['string','string'], [keyString, valueString]).replace("0x", "");
         const txData = {
